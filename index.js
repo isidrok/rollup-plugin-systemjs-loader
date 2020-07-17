324af33489e14e03f0fce678e899e9132efc5934
Isidro Torregrosa <isistorre95@gmail.com>
2020-07-17 12:03:00 +0200

    reflect loader on hashes

--- index.js
+++ index.js
@@ -2,28 +2,51 @@ const MagicString = require('magic-string');
 const fs = require('fs');
 
 function systemJSLoader(config) {
+    let contentLoading = false;
     let contentToInclude;
 
-    async function getContentToInclude(){
-        const include = await Promise.all(config.include.map((file) => { 
-            return fs.promises.readFile(file, 'utf8');
-        }));
-        contentToInclude = include.join('\n');
+    function before(content){
+        return `if(!window.System){ ${content} }`;
     }
-    
+
+    function after(fileName){
+        return `System.import('./${fileName}')`;
+    }
+
+    function saveContentToInclude() {
+        if (contentLoading) {
+            return;
+        }
+        contentLoading = true;
+        return Promise.all(config.include.map((file) => {
+            return fs.promises.readFile(file, 'utf8');
+        })).then((include) => {
+            contentToInclude = include.join('\n');
+        });
+    }
+
+
     return {
         name: 'systemjs-loader',
+        renderStart() {
+            return saveContentToInclude();
+        },
+        augmentChunkHash(chunk) {
+            if(chunk.isEntry){
+                return before(contentToInclude) + after(chunk.name);
+            }
+        },
         async renderChunk(code, chunk, options) {
             if (!chunk.isEntry) {
                 return null;
             }
-            if(!contentToInclude){
+            if (!contentToInclude) {
                 await getContentToInclude();
             }
             const magicString = new MagicString(code, { filename: chunk.fileName });
             magicString
-                .prepend(`if(!window.System){ ${contentToInclude} }`)
-                .append(`System.import('./${chunk.fileName}')`);
+                .prepend(before(contentToInclude))
+                .append(after(chunk.fileName));
             return {
                 code: magicString.toString(),
                 map: options.sourcemap ? magicString.generateMap() : null
